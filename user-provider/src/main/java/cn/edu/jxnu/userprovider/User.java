7be2080e379ad1d27bb5b784291ca7d91fb929c9
jxnu-liguobin <568845948@qq.com>
2018-04-18 23:14:43 +0800

    update

---
+++ user-provider/src/main/java/cn/edu/jxnu/userprovider/User.java
@@ -0,0 +1,28 @@
+package cn.edu.jxnu.userprovider;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author 梦境迷离
+ * @version V1.0 
+ * @time. 2018年4月18日
+ */
+@Data
+@ToString
+public class User {
+
+	private Integer id;
+
+	private String name;
+
+	public User(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public User() {
+		super();
+	}
+}
