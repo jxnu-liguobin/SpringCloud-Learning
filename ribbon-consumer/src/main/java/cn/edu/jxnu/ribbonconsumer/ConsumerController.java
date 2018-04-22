7facc0e4e458137d6a68b8c6c279bece1ecf849c
jxnu-liguobin <568845948@qq.com>
2018-04-22 12:40:19 +0800

    update

--- ribbon-consumer/src/main/java/cn/edu/jxnu/ribbonconsumer/ConsumerController.java
+++ ribbon-consumer/src/main/java/cn/edu/jxnu/ribbonconsumer/ConsumerController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package cn.edu.jxnu.ribbonconsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,51 +24,13 @@ public class ConsumerController {
 	public String helloConsumer() {
 		String string = restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
 		log.info("消费者:{}。。。。", string);
-		return "消费者："+string;
+		return "消费者：" + string;
 	}
 
 	@RequestMapping(value = "/user-consumer", method = RequestMethod.GET)
 	public String userConsumer() {
 		String user = restTemplate.getForEntity("http://USER-SERVICE/user", String.class).getBody();
 		log.info("消费者:{}。。。。", user);
-		return "消费者："+user;
+		return "消费者：" + user;
 	}
 }
-=======
-package cn.edu.jxnu.ribbonconsumer;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import lombok.extern.slf4j.Slf4j;
-
-/**
- * @author 梦境迷离
- * @description
- * @time 2018年4月5日
- */
-@Slf4j
-@RestController
-public class ConsumerController {
-
-	@Autowired
-	RestTemplate restTemplate;
-
-	@RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
-	public String helloConsumer() {
-		String string = restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
-		log.info("消费者:{}。。。。", string);
-		return "消费者："+string;
-	}
-
-	@RequestMapping(value = "/user-consumer", method = RequestMethod.GET)
-	public String userConsumer() {
-		String user = restTemplate.getForEntity("http://USER-SERVICE/user", String.class).getBody();
-		log.info("消费者:{}。。。。", user);
-		return "消费者："+user;
-	}
-}
->>>>>>> d46f2d713ed86d5411620a5faa41f5fb430f7a0b
--- user-provider/src/main/java/cn/edu/jxnu/userprovider/User.java
+++ user-provider/src/main/java/cn/edu/jxnu/userprovider/User.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package cn.edu.jxnu.userprovider;
 
 import lombok.Data;
@@ -6,8 +5,8 @@ import lombok.ToString;
 
 /**
  * @author 梦境迷离
- * @version V1.0 
- * @time. 2018年4月18日
+ * @version V1.0
+ * @time 018年4月18日
  */
 @Data
 @ToString
@@ -27,33 +26,3 @@ public class User {
 		super();
 	}
 }
-=======
-package cn.edu.jxnu.userprovider;
-
-import lombok.Data;
-import lombok.ToString;
-
-/**
- * @author 梦境迷离
- * @version V1.0 
- * @time. 2018年4月18日
- */
-@Data
-@ToString
-public class User {
-
-	private Integer id;
-
-	private String name;
-
-	public User(Integer id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-
-	public User() {
-		super();
-	}
-}
->>>>>>> d46f2d713ed86d5411620a5faa41f5fb430f7a0b
