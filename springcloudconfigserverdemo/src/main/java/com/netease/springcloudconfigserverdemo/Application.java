08071853a0ab6931564eb455f36eff3057899cee
李国斌 <liguobin@corp.netease.com>
2018-07-11 17:48:14 +0800

    修改文件名称

---
+++ springcloudconfigserverdemo/src/main/java/com/netease/springcloudconfigserverdemo/Application.java
@@ -0,0 +1,25 @@
+package com.netease.springcloudconfigserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 为了方便不再加分支的config版本控制
+ * 只记录匹配规则：其中label分支名，profile是环境名
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ * <p>
+ * config-repository文件是需要放在git上的配置文件
+ */
+@SpringBootApplication
+@EnableConfigServer//开启SpringCloud Config 服务端功能
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
